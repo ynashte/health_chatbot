9e222d1e43289643ae99c4403624d27e17895039
Yash Nashte <158258383+ynashte@users.noreply.github.com>
2025-09-13 22:35:47 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
-from app import app  # Import your Flask app instance from app.py
+from app import app
 
-# Vercel looks for this 'handler' function
 def handler(environ, start_response):
     return app.wsgi_app(environ, start_response)
